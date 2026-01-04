f6a39b513a1565a5df780d1ed01577f1c90db83e
Mosarof-Hossen <mmd58553@gmail.com>
2026-01-04 22:08:58 +0600

    first commit

---
+++ array-looping.js
@@ -0,0 +1,64 @@
+// Task 1
+// Write a JavaScript code to reverse the array colors without using the reverse method.
+// Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// Output:
+// ['orange', 'yellow', 'green', 'blue', 'red']
+
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+const color=[];
+for(let i=0; i<colors.length;i++){
+    color.unshift(colors[i])
+}
+console.log(color)
+
+
+// Task 2
+// Write a JavaScript code to get the even numbers from an array using any looping technique.
+// Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
+// Output:
+// [12, 98, 76, 46]
+
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+const evenNumber=[];
+for(let i=0;i<numbers.length;i++){
+    if(numbers[i]%2===0){
+        evenNumber.push(numbers[i])
+    }
+}
+console.log(evenNumber)
+
+
+// Task 3
+// Use a for...of loop to concatenate all the elements of an array into a single string.
+// Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+// Output:
+// 'TomTimTinTik'
+
+let addString = ['Tom', 'Tim', 'Tin', 'Tik'];
+console.log(addString.join(''))
+
+
+let result='';
+for(let i=0;i<addString.length;i++){
+    result=result.concat(addString[i],'')
+}
+result='"'+result+'"'
+console.log(result.trim(""))
+
+
+
+// Task 4 (Hard)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+// Input: const statement = 'I am a hard working person'
+// Output:
+// 'person working hard a am I'
+
+const statement = 'I am a hard working person'
+const ok=statement.split(' ').reverse()  .join(' '); ;
+console.log(ok )
+
+
+
+
+
+
