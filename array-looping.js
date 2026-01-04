1df02b8aabdb77b1a3ff200da2415cebdf607bd0
Mosarof-Hossen <mmd58553@gmail.com>
2026-01-04 22:23:08 +0600

    task-5 compilete

--- array-looping.js
+++ array-looping.js
@@ -4,13 +4,12 @@
 // Output:
 // ['orange', 'yellow', 'green', 'blue', 'red']
 
-const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
-const color=[];
-for(let i=0; i<colors.length;i++){
-    color.unshift(colors[i])
+const colors = ["red", "blue", "green", "yellow", "orange"];
+const color = [];
+for (let i = 0; i < colors.length; i++) {
+  color.unshift(colors[i]);
 }
-console.log(color)
-
+console.log(color);
 
 // Task 2
 // Write a JavaScript code to get the even numbers from an array using any looping technique.
@@ -19,14 +18,13 @@ console.log(color)
 // [12, 98, 76, 46]
 
 const numbers = [12, 98, 5, 41, 23, 78, 46];
-const evenNumber=[];
-for(let i=0;i<numbers.length;i++){
-    if(numbers[i]%2===0){
-        evenNumber.push(numbers[i])
-    }
+const evenNumber = [];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenNumber.push(numbers[i]);
+  }
 }
-console.log(evenNumber)
-
+console.log(evenNumber);
 
 // Task 3
 // Use a for...of loop to concatenate all the elements of an array into a single string.
@@ -34,18 +32,15 @@ console.log(evenNumber)
 // Output:
 // 'TomTimTinTik'
 
-let addString = ['Tom', 'Tim', 'Tin', 'Tik'];
-console.log(addString.join(''))
+let addString = ["Tom", "Tim", "Tin", "Tik"];
+console.log(addString.join(""));
 
-
-let result='';
-for(let i=0;i<addString.length;i++){
-    result=result.concat(addString[i],'')
+let result = "";
+for (let i = 0; i < addString.length; i++) {
+  result = result.concat(addString[i], "");
 }
-result='"'+result+'"'
-console.log(result.trim(""))
-
-
+result = '"' + result + '"';
+console.log(result.trim(""));
 
 // Task 4 (Hard)
 // Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
@@ -53,12 +48,22 @@ console.log(result.trim(""))
 // Output:
 // 'person working hard a am I'
 
-const statement = 'I am a hard working person'
-const ok=statement.split(' ').reverse()  .join(' '); ;
-console.log(ok )
+const statement = "I am a hard working person";
+const ok = statement.split(" ").reverse().join(" ");
+console.log(ok);
 
+// Task 5
+// Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+// Input: [1, 2, 3]
+// Expected Output:
+// Original: [1, 2, 3] Copy: [99, 2, 3]
 
+const inputArray = [1, 2, 3];
 
+const copyArray=[];
 
+copyArray.push(...inputArray);
+console.log(inputArray);
 
-
+copyArray[0]=99;
+console.log(copyArray)
