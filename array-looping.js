3bcde5712b15c3a75ff281f0db15c876346fa33e
Mosarof-Hossen <mmd58553@gmail.com>
2026-01-04 22:46:57 +0600

    all task compilete

--- array-looping.js
+++ array-looping.js
@@ -87,3 +87,22 @@ for(let i=0;i<studentMarks.length;i++){
     console.log(studentMarks[i].name+ " scored "+studentMarks[i].marks)
 }
 
+
+
+// Task 7
+// Given a 2D array, update the value at second row first item to 99 and print the updated array.
+// input:[
+//   [1, 2],
+//   [3, 4],
+//   [5, 6]
+// ]
+// Expected Array:[
+//   [1, 2],
+//   [99, 4],
+//   [5, 6]
+// ]
+
+let inputArray1=[[1, 2],[3, 4], [5, 6]]
+inputArray1[1][0]=99;
+console.log(...inputArray1)
+
